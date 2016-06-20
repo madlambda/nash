4470195b99c94134b22d9a288d251f8194066131
Tiago <tiago.natel@neoway.com.br>
2016-06-20 17:26:11 -0300

    vendor.sh nashfmt'ed

--- cmd/nash/vendor.sh
+++ cmd/nash/vendor.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env nash
-
 fn vendor() {
-   rm -rf vendor
-   mkdir -p vendor/bin vendor/src vendor/pkg
-
-   GOPATH <= pwd | xargs echo -n
-   GOPATH = $GOPATH + "/vendor"
-   setenv GOPATH
-
-   GOBIN = $GOPATH + "/bin"
-   setenv GOBIN
-
-   go get -v .
-
-   IFS = ("\n")
-   paths <= ls vendor/src
-
-   for path in $paths {
-       mv "vendor/src/" + $path vendor/
-   }
-
-   rm -rf vendor/src vendor/bin vendor/pkg
-
-   # because nash library is a dependency of cmd/nash
-   # we need to remove it at end
-   rm -rf vendor/github.com/NeowayLabs
+	rm -rf vendor
+	mkdir -p vendor/bin vendor/src vendor/pkg
+	GOPATH <= pwd | xargs echo -n
+	GOPATH = $GOPATH+"/vendor"
+	setenv GOPATH
+	GOBIN = $GOPATH+"/bin"
+	setenv GOBIN
+	go get -v .
+	IFS = ("
+		")
+	paths <= ls vendor/src
+	for path in $paths {
+	mv "vendor/src/"+$path vendor/
+	}
+	rm -rf vendor/src vendor/bin vendor/pkg
+	# because nash library is a dependency of cmd/nash
+	# we need to remove it at end
+	rm -rf vendor/github.com/NeowayLabs
 }
 
 vendor()
