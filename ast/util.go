4931060d9984db481d7dfc1587b598c53efb521d
Tiago <tiago.natel@neoway.com.br>
2016-08-24 09:40:15 -0300

    Remove unused function

--- ast/util.go
+++ ast/util.go
@@ -1,16 +1,8 @@
 package ast
 
-import (
-	"strings"
-
-	"github.com/NeowayLabs/nash/token"
-)
+import "strings"
 
 func stringify(s string) string {
 	return strings.Replace(strings.Replace(s, "\n", "\\n", -1),
 		"\t", "\\t", -1)
-}
-
-func NewSimpleArg(pos token.Pos, n string, quoted bool) Expr {
-	return NewStringExpr(pos, n, quoted)
 }
