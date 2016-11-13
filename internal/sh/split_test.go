31681f617a7bef85c181ca8e2a5c40efdfac3716
Tiago <tiago.natel@neoway.com.br>
2016-11-12 22:07:53 -0200

    add tests for splitfn by list and func
    Signed-off-by: Tiago <tiago.natel@neoway.com.br>

--- internal/sh/split_test.go
+++ internal/sh/split_test.go
@@ -106,5 +106,101 @@ func TestSplitFnBasic(t *testing.T) {
 			},
 			sep: sh.NewStrObj("\n"),
 		},
+		{
+			content: "some thing\nwith\nnew\nlines",
+			expected: []sh.Obj{
+				sh.NewStrObj("some"),
+				sh.NewStrObj("thing"),
+				sh.NewStrObj("with"),
+				sh.NewStrObj("new"),
+				sh.NewStrObj("lines"),
+			},
+			sep: sh.NewListObj([]sh.Obj{
+				sh.NewStrObj(" "),
+				sh.NewStrObj("\n"),
+			}),
+		},
 	})
+}
+
+func TestSplitFnByFunc(t *testing.T) {
+	content := `plan9;linux,osx,windows`
+	expected := []sh.Obj{
+		sh.NewStrObj("plan9"),
+		sh.NewStrObj("linux"),
+		sh.NewStrObj("osx"),
+		sh.NewStrObj("windows"),
+	}
+
+	shell, err := NewShell()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = shell.Exec("TestSplitByFunc", `
+fn tolist(char) {
+        if $char == ";" {
+            return "0"
+        } else if $char == "," {
+            return "0"
+        }
+
+        return "1"
+}`)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fn, ok := shell.GetFn("tolist")
+
+	if !ok {
+		t.Fatalf("Function tolist not declared")
+	}
+
+	splitfn := NewSplitFn(shell)
+	splitfn.SetArgs([]sh.Obj{
+		sh.NewStrObj(content),
+		sh.NewFnObj(fn),
+	})
+
+	err = splitfn.Start()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = splitfn.Wait()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result := splitfn.Results()
+
+	if result.Type() != sh.ListType {
+		t.Fatalf("Splitfn returns wrong type")
+	}
+
+	values := result.(*sh.ListObj).List()
+
+	if len(values) != len(expected) {
+		t.Fatalf("Expected %d values, but got %d",
+			len(expected), len(values))
+	}
+
+	for i := 0; i < len(values); i++ {
+		if values[i].Type() != sh.StringType {
+			t.Fatalf("Split must return list of strings %v",
+				values[i])
+		}
+
+		v := values[i].(*sh.StrObj).Str()
+		e := expected[i].(*sh.StrObj).Str()
+
+		if v != e {
+			t.Fatalf("Values differ: '%s' != '%s'", e, v)
+		}
+	}
 }
