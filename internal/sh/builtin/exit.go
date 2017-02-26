87c8f53d0881d3582243c4e5bb0c50700fb5953c
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-02-26 11:14:33 -0300

    Add impl to unsupported OS'ses

---
+++ internal/sh/builtin/exit.go
@@ -0,0 +1,37 @@
+//+build !linux
+
+package builtin
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/NeowayLabs/nash/sh"
+)
+
+type (
+	exitNotSupportedFn struct {
+		err error
+	}
+)
+
+func newExit() *exitNotSupportedFn {
+	return &exitNotSupportedFn{
+		err: fmt.Errorf("exit is not implemented on OS: %s", runtime.GOOS),
+	}
+}
+
+func (e *exitNotSupportedFn) ArgNames() []string {
+	panic(e.err)
+	return []string{}
+}
+
+func (e *exitNotSupportedFn) Run() (sh.Obj, error) {
+	panic(e.err)
+	return nil, nil //Unrecheable code
+}
+
+func (e *exitNotSupportedFn) SetArgs(args []sh.Obj) error {
+	panic(e.err)
+	return nil
+}
