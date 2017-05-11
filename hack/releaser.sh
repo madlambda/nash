4846ab7e11ccc7107c4b9315a21fb064a1b44968
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-05-11 13:57:59 -0300

    Add version to binary

--- hack/releaser.sh
+++ hack/releaser.sh
@@ -5,23 +5,28 @@ if len($ARGS) != "2" {
 	exit("1")
 }
 
-version = $ARGS[1]
+version      = $ARGS[1]
 supported_os = ("linux" "darwin" "windows")
 
 # Guarantee passing tests at least on the host arch/os
 make test
 
-setenv CGO_ENABLED="0"
+setenv CGO_ENABLED = "0"
+
 mkdir -p dist
 
 for os in $supported_os {
-    setenv GOOS = $os
-    setenv GOARCH = "amd64"
-    echo "building OS: " + $GOOS + " ARCH : " + $GOARCH
-    make build "version=" + $version
-    if $os != "windows" {
-        cp "cmd/nash/nash" "dist/nash-"+$os+"-amd64"
-    } else {
-        cp cmd/nash/nash.exe "dist/nash-"+$os+"-amd64.exe"
-    }
+	setenv GOOS = $os
+	setenv GOARCH = "amd64"
+
+	echo "building OS: "+$GOOS+" ARCH : "+$GOARCH
+	make build "version="+$version
+
+	binpath <= format("dist/nash-%s-%s-amd64", $version, $os)
+
+	if $os != "windows" {
+		binpath = $binpath+".exe"
+	}
+
+	cp "cmd/nash/nash" $binpath
 }
