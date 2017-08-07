36af313b54c8979d1aa60e737e62a3468d220c77
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-08-07 19:07:27 -0300

    Small improvement

--- hack/install/linux.sh
+++ hack/install/linux.sh
@@ -3,10 +3,15 @@
 set -o errexit
 set -o nounset
 
-VERSION="v0.5"
-echo "installing nash: "$VERSION" at NASHPATH: "$NASHPATH
+if [ -z "$NASHROOT" ]; then
+        NASHROOT=$HOME/nashroot
+fi
 
-cd $NASHPATH
+VERSION="v0.5"
+echo "installing nash: "$VERSION" at NASHROOT: "$NASHROOT
+
+mkdir -p $NASHROOT
+cd $NASHROOT
 tarfile="nash-$VERSION-linux-amd64.tar.gz"
 wget https://github.com/NeowayLabs/nash/releases/download/$VERSION/$tarfile
 tar xvfz $tarfile
