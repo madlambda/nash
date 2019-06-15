880c9e15ba794759cacd35c5ecc18dd0a498c992
Tiago de Bem <tiago.moura@nginx.com>
2019-06-15 22:32:30 +0100

    add missing files
    Signed-off-by: Tiago de Bem <tiago.moura@nginx.com>

--- hack/install/unix.sh
+++ hack/install/unix.sh
@@ -2,22 +2,26 @@
 
 set -o errexit
 set -o nounset
+set -o pipefail
 
-if [ -z "$NASHROOT" ]; then
-        NASHROOT=$HOME/nashroot
-fi
+which wget >/dev/null   || { echo "wget not installed"; exit 1; } 
+which tar >/dev/null    || { echo "tar not installed"; exit 1; }
+which tr >/dev/null     || { echo "tr not found"; exit 1; }
 
-VERSION="v0.6"
+NASHROOT=${NASHROOT:-$HOME/nashroot}
+VERSION="v1.1"
+ARCH="amd64"
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
 
 if [ $# -eq 1 ]; then
         VERSION=$1
 fi
 
-echo "installing nash: "$VERSION" at NASHROOT: "$NASHROOT
+echo "installing nash (${OS}): ${VERSION} at NASHROOT: ${NASHROOT}"
 
 mkdir -p $NASHROOT
 cd $NASHROOT
-tarfile="nash-$VERSION-linux-amd64.tar.gz"
-wget https://github.com/NeowayLabs/nash/releases/download/$VERSION/$tarfile
+tarfile="nash-${VERSION}-${OS}-${ARCH}.tar.gz"
+wget -c https://github.com/NeowayLabs/nash/releases/download/$VERSION/$tarfile
 tar xvfz $tarfile
 rm -f $tarfile
